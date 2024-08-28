27b24e49a3a048ff59aed7541cab0aaff32bb6c7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 09:23:06 -0300

    [ :gear: ] | create: config - cypress (app::config)

---
+++ tutorialls/cypress.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "cypress";
+import { env } from "process";
+
+export default defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  video: true,
+  screenshotOnRunFailure: true,
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",
+  downloadsFolder: "cypress/downloads",
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: env.API_URL || "http://localhost:3000",
+    specPattern: [
+      "test/app/E2E/**/*.cy.{js,jsx,ts,tsx}",
+      "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+      "cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
+      "cypress/E2E/**/*.cy.{js,jsx,ts,tsx}",
+    ],
+  },
+});
