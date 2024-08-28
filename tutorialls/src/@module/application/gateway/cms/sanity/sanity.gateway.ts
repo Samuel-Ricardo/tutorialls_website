eb3d5ec61b1621019d79a666424e9ef5227cae10
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 14:11:38 -0300

    [ :door: ] | implements: gateway - cms > getString [sanity] (module::application)

--- tutorialls/src/@module/application/gateway/cms/sanity/sanity.gateway.ts
+++ tutorialls/src/@module/application/gateway/cms/sanity/sanity.gateway.ts
@@ -1,18 +1,22 @@
 import { inject, injectable } from 'inversify';
 import { ICMSGateway } from '../../../../infra/engine/gateway/cms/cms.gateway';
-import { type SANITY_CLIENT_TYPE } from '@/@type/module/infra/engine/gateway/cms/sanity.type';
 import { MODULE } from '@/@module/app.registry';
 import { IGetFromCMSDTO } from '@/@module/domain/DTO/infra/engine/cms/get.dto';
+import { SanityClient } from 'sanity';
 
 @injectable()
 export class SanityCMSGateway implements ICMSGateway {
   constructor(
     @inject(MODULE.INFRA.ENGINE.GATEWAY.CMS.SANITY)
-    private readonly engine: SANITY_CLIENT_TYPE,
+    private readonly engine: SanityClient,
   ) {}
 
-  async getString(DTO: IGetFromCMSDTO) {
-    return 'string';
+  async getString({ id }: IGetFromCMSDTO) {
+    const query = `*[_type == "paragraph" && identifier == $identifier][0]{ body }`;
+    const params = { identifier: id };
+    const result = await this.engine.fetch(query, params);
+    console.log({ result });
+    return result.body;
   }
   async getImage(DTO: IGetFromCMSDTO) {
     return 'image';
--- tutorialls/src/@module/infra/engine/engine.factory.ts
+++ tutorialls/src/@module/infra/engine/engine.factory.ts
@@ -1,16 +1,14 @@
-import { SANITY_CLIENT_TYPE } from '@/@type/module/infra/engine/gateway/cms/sanity.type';
 import { ENGINE_MODULE } from './engine.module';
 import { ENGINE_REGISTRY } from './engine.registry';
 import { AxiosHttpGateway } from './gateway/http/axios/axios.gateway';
 import { HttpNodeEngine } from './gateway/http/node/node.gateway';
+import { SanityClient } from 'sanity';
 
 export const ENGINE_FACTORY = {
   GATEWAY: {
     CMS: {
       SANITY: () =>
-        ENGINE_MODULE.get<SANITY_CLIENT_TYPE>(
-          ENGINE_REGISTRY.GATEWAY.CMS.SANITY,
-        ),
+        ENGINE_MODULE.get<SanityClient>(ENGINE_REGISTRY.GATEWAY.CMS.SANITY),
     },
     HTTP: {
       NODE: () =>
