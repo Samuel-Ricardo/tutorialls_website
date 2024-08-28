c9d7a22f09e6c01cf47ff6dd026dbd5ebedbce47
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 09:27:10 -0300

    [ :gear: ] | update: config - cypress (app::config::test)

--- tutorialls/cypress.config.ts
+++ tutorialls/cypress.config.ts
@@ -4,30 +4,31 @@ import { env } from "process";
 export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
+
   video: true,
+  videoCompression: true,
   screenshotOnRunFailure: true,
+
   videosFolder: "cypress/videos",
   screenshotsFolder: "cypress/screenshots",
   downloadsFolder: "cypress/downloads",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: env.APP_URL || "http://localhost:3000",
+    specPattern: [
+      "cypress/E2E/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+      "cypress/integration/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+    ],
+  },
 
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
     },
-    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: env.API_URL || "http://localhost:3000",
-    specPattern: [
-      "test/app/E2E/**/*.cy.{js,jsx,ts,tsx}",
-      "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-      "cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
-      "cypress/E2E/**/*.cy.{js,jsx,ts,tsx}",
-    ],
+    specPattern: "cypress/component/**/*.{cy,spec}.{js,jsx,ts,tsx}",
   },
 });
