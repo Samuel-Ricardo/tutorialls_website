dfa2835f80306822d2c300dd783d1e0cd4f27a4d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 11:56:08 -0300

    [ :syringe: ] | create: factory - config (module::infra)

--- tutorialls/src/@module/infra/config/config.factory.ts
+++ tutorialls/src/@module/infra/config/config.factory.ts
@@ -1,5 +1,5 @@
-import { CONFIG_TYPE } from '../../../@types/module/infra/config/config.type';
-import { ENV_TYPE } from '../../../@types/module/infra/config/env.type';
+import { CONFIG_TYPE } from '@/@type/module/infra/config/config.type';
+import { ENV_TYPE } from '@/@type/module/infra/config/env.type';
 import { CONFIG_MODULE } from './config.module';
 import { CONFIG_REGISTRY } from './config.registry';
 
@@ -8,5 +8,14 @@ export const CONFIG_FACTORY = {
   ENV: CONFIG_MODULE.get<ENV_TYPE>(CONFIG_REGISTRY.ENV),
   API: {
     URL: CONFIG_MODULE.get<string | undefined>(CONFIG_REGISTRY.API.URL),
+  },
+  ENCRYPTION: {
+    KEY: CONFIG_MODULE.get<string | undefined>(CONFIG_REGISTRY.ENCRYPTION.KEY),
+    ALGORITHM: CONFIG_MODULE.get<string | undefined>(
+      CONFIG_REGISTRY.ENCRYPTION.ALGORITHM,
+    ),
+    BREAKPOINT: CONFIG_MODULE.get<string | undefined>(
+      CONFIG_REGISTRY.ENCRYPTION.BREAKPOINT,
+    ),
   },
 };
