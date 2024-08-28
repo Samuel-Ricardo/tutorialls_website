0d18e750da50f84f067441c9ce80a5cdceaea8c5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 13:34:44 -0300

    [ :key: ] | update: config - env (module::infra)

--- tutorialls/src/@module/infra/config/env/env.config.ts
+++ tutorialls/src/@module/infra/config/env/env.config.ts
@@ -8,4 +8,13 @@ export const ENV = {
     ALGORITHM: process.env.NEXT_PUBLIC_ENCRYPTION_ALGORITHM || 'aes-256-ctr',
     BREAKPOINT: process.env.NEXT_PUBLIC_ENCRYPTION_BREAKPOINT || ':',
   },
+  SANITY: {
+    PROJECT: {
+      ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'ProjectId',
+    },
+    DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET || 'development',
+    USE: {
+      CDN: process.env.NEXT_PUBLIC_SANITY_USE_CDN === 'true' || true,
+    },
+  },
 };
