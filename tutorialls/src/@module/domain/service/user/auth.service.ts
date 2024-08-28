19cf7da726e27960a5a392ac0e103083d0a62b4f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 19:03:43 -0300

    [ :passport_control: ] | update: service - auth > decode [user] (module::domain)

--- tutorialls/src/@module/domain/service/user/auth.service.ts
+++ tutorialls/src/@module/domain/service/user/auth.service.ts
@@ -1,7 +1,10 @@
+import { IUserDTO } from '../../DTO/user.dto';
+import { IDecodeUserDTO } from '../../DTO/user/decode.dto';
 import { ILoginUserDTO } from '../../DTO/user/login.dto';
 import { ISignupUserDTO } from '../../DTO/user/signup.dto';
 
 export interface IAuthService {
   login(user: ILoginUserDTO): Promise<string>;
   signup(user: ISignupUserDTO): Promise<boolean>;
+  decode(user: IDecodeUserDTO): Promise<IUserDTO>;
 }
