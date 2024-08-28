e940dc29d70a48adcd0978346c2d1a967fe4ff69
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 14:30:53 -0300

    [ :syringe: ] | update: module - app [application] (app::module)

--- tutorialls/src/@module/app.module.ts
+++ tutorialls/src/@module/app.module.ts
@@ -2,9 +2,14 @@ import 'reflect-metadata';
 
 import { Container } from 'inversify';
 import { INFRA_MODULE } from './infra/infra.module';
+import { APPLICATION_MODULE } from './application/application.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
 });
 
-export const APP_MODULE = Container.merge(_MODULE, INFRA_MODULE);
+export const APP_MODULE = Container.merge(
+  _MODULE,
+  INFRA_MODULE,
+  APPLICATION_MODULE,
+);
--- tutorialls/src/@module/app.registry.ts
+++ tutorialls/src/@module/app.registry.ts
@@ -1,5 +1,7 @@
+import { APPLICATION_REGISTRY } from './application/application.registry';
 import { INFRA_REGISTRY } from './infra/infra.registry';
 
 export const MODULE = {
   INFRA: INFRA_REGISTRY,
+  APPLICATION: APPLICATION_REGISTRY,
 };
