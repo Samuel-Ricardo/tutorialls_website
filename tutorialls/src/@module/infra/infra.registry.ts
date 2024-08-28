c5c2850cd1bb7527837ebbf55961a1aae585c896
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 12:03:53 -0300

    [ :syringe: ] | create: registry - infra (app::module)

---
+++ tutorialls/src/@module/infra/infra.registry.ts
@@ -0,0 +1,7 @@
+import { CONFIG_REGISTRY } from './config/config.registry';
+import { ENGINE_REGISTRY } from './engine/engine.registry';
+
+export const INFRA_REGISTRY = {
+  ENGINE: ENGINE_REGISTRY,
+  CONFIG: CONFIG_REGISTRY,
+};
