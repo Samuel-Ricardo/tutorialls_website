e05daf82b4990bd1e4b7493d0e85bb7f2e89ac2e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 15:58:08 -0300

    [ :white_check_mark: ] | create: test - gateway > sanity [cms] (module::application)

---
+++ tutorialls/test/modules/application/gateway/cms/sanity.gateway.spec.ts
@@ -0,0 +1,29 @@
+/*
+ * @jest-environment node
+ */
+
+import 'reflect-metadata';
+
+import { MODULES } from '@/@module/app.facotry';
+import { ICMSGateway } from '@/@module/infra/engine/gateway/cms/cms.gateway';
+import { SanityCMSGateway } from '@/@module/application/gateway/cms/sanity/sanity.gateway';
+import { EXPECTED_SANITY_PARAGRAPH } from '../../../../data/sanity.data';
+
+describe('[INTEGRATION] | GATEWAY => [SANITY] | [CMS]', () => {
+  let gateway: ICMSGateway;
+
+  beforeAll(() => {
+    gateway = MODULES.APPLICATION.GATEWAY.CMS.SANITY();
+
+    expect(gateway).toBeDefined();
+    expect(gateway).toBeInstanceOf(SanityCMSGateway);
+  });
+
+  test('should return a string', async () => {
+    const result = await gateway.getString({
+      id: '1',
+    });
+
+    expect(result).toEqual(EXPECTED_SANITY_PARAGRAPH);
+  });
+});
