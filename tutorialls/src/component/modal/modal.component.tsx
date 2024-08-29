42b5bb14d63bf690f763347c3a9070e2a36f7a58
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-29 12:40:38 -0300

    [ :art: ] | create: modal (app::page)

--- tutorialls/src/component/modal/modal.component.tsx
+++ tutorialls/src/component/modal/modal.component.tsx
@@ -1,23 +1,32 @@
-'use client';
-
-import useSignupModalStore from '@/store/modal/signup.store';
 import { IoCloseOutline } from 'react-icons/io5';
 
-export const SignupModal = () => {
-  const { isOpen, closeModal } = useSignupModalStore();
+export const Modal = ({
+  isOpen,
+  close,
+  children,
+}: {
+  isOpen: boolean;
+  close: () => void;
+  children: React.ReactNode;
+}) => {
   if (!isOpen) return null;
 
   return (
     <div
-      onClick={closeModal}
+      onClick={close}
       className="fixed flex justify-center items-center h-screen w-screen bg-[#76ff0208] backdrop-blur-lg top-0 left-0"
     >
       <IoCloseOutline
-        onClick={closeModal}
+        onClick={close}
         className="text-[#76ff02] cursor-pointer hover:scale-150 text-4xl ease-in-out duration-300 fixed top-10 right-10"
       />
 
-      <div className="bg-black/90 h-fit w-fit"></div>
+      <div
+        onClick={e => e.stopPropagation()}
+        className="bg-black/90 h-fit w-fit p-8 rounded-lg"
+      >
+        {children}
+      </div>
     </div>
   );
 };
