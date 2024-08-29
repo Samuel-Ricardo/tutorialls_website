dd61374575cad56e51879670ede6563999b2e174
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 21:54:31 -0300

    [ :passport_control: ] | update: service - auth > decode [user] (module::domain)

--- tutorialls/src/@module/application/service/user/auth.service.ts
+++ tutorialls/src/@module/application/service/user/auth.service.ts
@@ -1,8 +1,10 @@
 import { MODULE } from '@/@module/app.registry';
+import { IDecodeUserDTO } from '@/@module/domain/DTO/user/decode.dto';
 import { ILoginUserDTO } from '@/@module/domain/DTO/user/login.dto';
 import { ISignupUserDTO } from '@/@module/domain/DTO/user/signup.dto';
 import { IAuthService } from '@/@module/domain/service/user/auth.service';
 import type { ILoginUserUseCase } from '@/@module/domain/use_case/user/login.use_case';
+import type { IDecodeUserUseCase } from '@/@module/domain/use_case/user/security/decode.use_case';
 import type { ISignupUserUseCase } from '@/@module/domain/use_case/user/signup.use_case';
 import { inject, injectable } from 'inversify';
 
@@ -13,6 +15,8 @@ export class AuthService implements IAuthService {
     private readonly loginUser: ILoginUserUseCase,
     @inject(MODULE.APPLICATION.USE_CASE.USER.SIGNUP)
     private readonly signupUser: ISignupUserUseCase,
+    @inject(MODULE.APPLICATION.USE_CASE.USER.SECURITY.DECODE)
+    private readonly decodeUser: IDecodeUserUseCase,
   ) {}
 
   async login(user: ILoginUserDTO) {
@@ -21,5 +25,9 @@ export class AuthService implements IAuthService {
 
   async signup(user: ISignupUserDTO) {
     return await this.signupUser.execute(user);
+  }
+
+  async decode({ token }: IDecodeUserDTO) {
+    return await this.decodeUser.execute({ token });
   }
 }
