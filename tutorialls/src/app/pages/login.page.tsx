2c8fc8e726a9282d57b710f553f30d0636e49ffb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 23:03:48 -0300

    [ :page_with_curl: ] | create: page - login (app::page)

--- tutorialls/src/app/pages/login.page.tsx
+++ tutorialls/src/app/pages/login.page.tsx
@@ -1,9 +1,13 @@
+import { LoginForm } from '@/component/form/login.form';
+
 export const LoginPage = () => {
   return (
-    <main className="flex flex-col min-h-screen min-w-screen bg-[#171717]">
+    <main className="flex flex-col min-h-screen min-w-screen bg-[#171717] p-5">
       <h1 className="text-[#73eb12] text-6xl font-bold text-center mb-24">
         Login
       </h1>
+
+      <LoginForm />
     </main>
   );
 };
