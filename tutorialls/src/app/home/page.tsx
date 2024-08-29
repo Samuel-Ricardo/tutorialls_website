82a6d12b9f99c0cdb6c4615b67b9649abc29a551
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 21:57:20 -0300

    [ :art: ] | setup: page - home (app::page)

---
+++ tutorialls/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen min-w-screen bg-[#171717] justify-center items-center">
+      <h1 className="text-[#73eb12] text-4xl font-extrabold text-center mb-24">
+        WELCOME TO TUTORIALLS
+      </h1>
+    </main>
+  );
+}
---
+++ tutorialls/src/app/pages/login.page.tsx
@@ -0,0 +1,9 @@
+export const LoginPage = () => {
+  return (
+    <main className="flex flex-col min-h-screen min-w-screen bg-[#171717]">
+      <h1 className="text-[#73eb12] text-6xl font-bold text-center mb-24">
+        Login
+      </h1>
+    </main>
+  );
+};
