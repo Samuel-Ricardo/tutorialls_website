69b334cc1d14fb9fd1e919ec388429a263961787
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-29 12:36:04 -0300

    [ :art: ] | create: component - h1 (app::component)

---
+++ tutorialls/src/component/typography/h1.component.tsx
@@ -0,0 +1,7 @@
+export const H1 = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <h1 className="text-[#73eb12] text-6xl font-bold text-center mb-24">
+      {children}
+    </h1>
+  );
+};
