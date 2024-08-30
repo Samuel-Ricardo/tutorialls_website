4a6206ac44fb17f62bef230652e9f92eb0f27176
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-30 15:32:51 -0300

    [ :sparkles: ] | implements: use-case - filter by [author] > tutorial (module::domain)

---
+++ tutorialls/src/@module/application/use_case/tutorial/filter/by/author.use_case.ts
@@ -0,0 +1,12 @@
+import { AxiosHttpTutorialGatewaySupport } from '@/@module/application/support/gateway/http/tutorial/tutorial.support';
+import { IFilterTutorialsByAuthorUseCase } from '@/@module/domain/use_case/tutorials/filter/by/author.use_case';
+import { IFilterTutorialsByAuthorDTO } from '@/tutorial/filter/by/author.dto';
+
+export class FilterTutorialByAuthorUseCase
+  extends AxiosHttpTutorialGatewaySupport
+  implements IFilterTutorialsByAuthorUseCase
+{
+  async execute(DTO: IFilterTutorialsByAuthorDTO) {
+    return this.gateway.findByAuthor(DTO);
+  }
+}
