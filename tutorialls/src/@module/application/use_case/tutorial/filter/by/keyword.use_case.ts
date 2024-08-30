670b230ae14bd962069fdf71a9bcea0c6bf7a858
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-30 15:34:06 -0300

    [ :sparkles: ] | implements: use-case - filter by [keyword] > tutorial (module::domain)

---
+++ tutorialls/src/@module/application/use_case/tutorial/filter/by/keyword.use_case.ts
@@ -0,0 +1,12 @@
+import { AxiosHttpTutorialGatewaySupport } from '@/@module/application/support/gateway/http/tutorial/tutorial.support';
+import { IFilterTutorialsByKeywordUseCase } from '@/@module/domain/use_case/tutorials/filter/by/keyword.use_case';
+import { IFilterTutorialsByContentDTO } from '@/tutorial/filter/by/content.dto';
+
+export class FilterTutorialByKeywordUseCase
+  extends AxiosHttpTutorialGatewaySupport
+  implements IFilterTutorialsByKeywordUseCase
+{
+  async execute(DTO: IFilterTutorialsByContentDTO) {
+    return this.gateway.findByKeywordInContent(DTO);
+  }
+}
