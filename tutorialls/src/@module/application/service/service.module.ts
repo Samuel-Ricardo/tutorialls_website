6d01fa4ae12cbd12581ec0558d4a701316a6a3f8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-30 15:53:47 -0300

    [ :syringe: ] | update: module - service [tutorial] (module::application)

--- tutorialls/src/@module/application/service/service.module.ts
+++ tutorialls/src/@module/application/service/service.module.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { AuthService } from './user/auth.service';
 import { SERVICE_REGISTRY } from './service.registry';
 import { USE_CASE_MODULE } from '../use_case/use_case.module';
+import { TutorialService } from './tutorial/tutorial.service';
 
 export const _MODULE = new Container({
   autoBindInjectable: true,
@@ -11,3 +12,4 @@ export const _MODULE = new Container({
 export const SERVICE_MODULE = Container.merge(_MODULE, USE_CASE_MODULE);
 
 SERVICE_MODULE.bind(SERVICE_REGISTRY.USER.AUTH).to(AuthService);
+SERVICE_MODULE.bind(SERVICE_REGISTRY.TUTORIAL).to(TutorialService);
