ebeeb40ca52225d47d7752c86f77d80e4b4693ec
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-30 15:41:53 -0300

    [ :syringe: ] | update: module - use-case [tutorial] (app::module)

--- tutorialls/src/@module/application/use_case/use_case.factory.ts
+++ tutorialls/src/@module/application/use_case/use_case.factory.ts
@@ -1,3 +1,10 @@
+import { CreateTutorialUseCase } from './tutorial/create.use_case';
+import { DeleteTutorialUseCase } from './tutorial/delete.use_case';
+import { FilterTutorialByAuthorUseCase } from './tutorial/filter/by/author.use_case';
+import { FilterTutorialByKeywordUseCase } from './tutorial/filter/by/keyword.use_case';
+import { FilterTutorialByTitleUseCase } from './tutorial/filter/by/title.use_case';
+import { ListAllTutorialsUseCase } from './tutorial/list/all.use_case';
+import { UpdateTutorialUseCase } from './tutorial/update.uce_case';
 import { USE_CASE_MODULE } from './use_case.module';
 import { USE_CASE_REGISTRY } from './use_case.registry';
 import { LoginUserUseCase } from './user/login.use_case';
@@ -15,6 +22,42 @@ export const USE_CASE_FACTORY = {
         USE_CASE_MODULE.get<DecodeUserUseCase>(
           USE_CASE_REGISTRY.USER.SECURITY.DECODE,
         ),
+    },
+  },
+  TUTORIAL: {
+    CREATE: () =>
+      USE_CASE_MODULE.get<CreateTutorialUseCase>(
+        USE_CASE_REGISTRY.TUTORIAL.CREATE,
+      ),
+    UPDATE: () =>
+      USE_CASE_MODULE.get<UpdateTutorialUseCase>(
+        USE_CASE_REGISTRY.TUTORIAL.UPDATE,
+      ),
+    DELETE: () =>
+      USE_CASE_MODULE.get<DeleteTutorialUseCase>(
+        USE_CASE_REGISTRY.TUTORIAL.DELETE,
+      ),
+    LIST: {
+      ALL: () =>
+        USE_CASE_MODULE.get<ListAllTutorialsUseCase>(
+          USE_CASE_REGISTRY.TUTORIAL.LIST.ALL,
+        ),
+    },
+    FILTER: {
+      BY: {
+        TITLE: () =>
+          USE_CASE_MODULE.get<FilterTutorialByTitleUseCase>(
+            USE_CASE_REGISTRY.TUTORIAL.FILTER.BY.TITLE,
+          ),
+        CONTENT: () =>
+          USE_CASE_MODULE.get<FilterTutorialByKeywordUseCase>(
+            USE_CASE_REGISTRY.TUTORIAL.FILTER.BY.CONTENT,
+          ),
+        AUTHOR: () =>
+          USE_CASE_MODULE.get<FilterTutorialByAuthorUseCase>(
+            USE_CASE_REGISTRY.TUTORIAL.FILTER.BY.AUTHOR,
+          ),
+      },
     },
   },
 };
--- tutorialls/src/@module/application/use_case/use_case.registry.ts
+++ tutorialls/src/@module/application/use_case/use_case.registry.ts
@@ -6,4 +6,23 @@ export const USE_CASE_REGISTRY = {
       DECODE: Symbol.for('MODULE::APP::USE_CASE::USER::SECURITY::DECODE'),
     },
   },
+  TUTORIAL: {
+    CREATE: Symbol.for('MODULE::APP::USE_CASE::TUTORIAL::CREATE'),
+    UPDATE: Symbol.for('MODULE::APP::USE_CASE::TUTORIAL::UPDATE'),
+    DELETE: Symbol.for('MODULE::APP::USE_CASE::TUTORIAL::DELETE'),
+    LIST: {
+      ALL: Symbol.for('MODULE::APP::USE_CASE::TUTORIAL::LIST::ALL'),
+    },
+    FILTER: {
+      BY: {
+        TITLE: Symbol.for('MODULE::APP::USE_CASE::TUTORIAL::FILTER::BY::TITLE'),
+        CONTENT: Symbol.for(
+          'MODULE::APP::USE_CASE::TUTORIAL::FILTER::BY::CONTENT',
+        ),
+        AUTHOR: Symbol.for(
+          'MODULE::APP::USE_CASE::TUTORIAL::FILTER::BY::AUTHOR',
+        ),
+      },
+    },
+  },
 };
