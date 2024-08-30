bd169cffde21e83b1af8ccb2d94334343f8e505d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-30 14:33:03 -0300

    [ :label: ] | create: DTO's - tutorial (module::domain)

--- tutorialls/src/app/tutorials/page.tsx
+++ tutorialls/src/app/tutorials/page.tsx
@@ -1,13 +1,18 @@
+import { LazyParagraph } from '@/component/lazy/paragraph.component';
+import { H1 } from '@/component/typography/h1.component';
 import { AuthWall } from '@/component/wall/auth.wall';
 
 export default function Tutorials() {
   return (
-    <AuthWall>
-      <main className="flex min-h-screen min-w-screen bg-[#171717] justify-center items-center">
-        <h1 className="text-[#73eb12] text-4xl font-extrabold text-center mb-24">
-          WELCOME TO TUTORIALLS
-        </h1>
-      </main>
-    </AuthWall>
+    //    <AuthWall>
+    <main className="flex min-h-screen min-w-screen bg-[#171717] justify-center items-center">
+      <H1>
+        <LazyParagraph
+          id="h1_tutorials_welcome"
+          defaultValue="WELCOME TO TUTORIALLS"
+        />
+      </H1>
+    </main>
+    //    </AuthWall>
   );
 }
